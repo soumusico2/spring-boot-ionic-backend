8c9ea52401469c8b085c923af3e6821ceec18917
Hebert Nunes <hebertcunha1@gmail.com>
2020-04-27 18:07:49 -0300

    primeiro commit

---
+++ src/main/java/com/hebertnunes/cursomvc/CursomvcApplication.java
@@ -0,0 +1,13 @@
+package com.hebertnunes.cursomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hebertnunes/cursomvc/CursomvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hebertnunes.cursomvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
