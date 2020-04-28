4a160fb408c00a6c48facf8f1394c649ebef3231
Hebert Nunes <hebertcunha1@gmail.com>
2020-04-28 12:35:35 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/hebertnunes/cursomvc/resource/CategoriaResource.java
+++ src/main/java/com/hebertnunes/cursomvc/resource/CategoriaResource.java
@@ -21,8 +21,6 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
 		Categoria categoria = service.buscar(id);
-		
-		
 		return ResponseEntity.ok().body(categoria);
 	}
 	
--- src/main/java/com/hebertnunes/cursomvc/services/CategoriaService.java
+++ src/main/java/com/hebertnunes/cursomvc/services/CategoriaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hebertnunes.cursomvc.domain.Categoria;
+import com.hebertnunes.cursomvc.exceptions.ObjectNotFoundException;
 import com.hebertnunes.cursomvc.repositores.CategoriaRepository;
 
 @Service
@@ -14,6 +15,12 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria categoria = categoriaRepository.findOne(id);
+		
+		if(categoria == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo " + Categoria.class.getName());
+		}
+		
 		return categoria;
 	}
 }
